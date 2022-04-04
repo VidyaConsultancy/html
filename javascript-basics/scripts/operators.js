0a2e415035310d7fb928ef48ed0f0a8340828a96
Arpit <arpit_jain@live.com>
2022-04-04 11:11:52 +0530

    feat: javascript operators

---
+++ javascript-basics/scripts/operators.js
@@ -0,0 +1,20 @@
+/**
+ * Addition +
+ * Substract -
+ * Multiply *
+ * Divide /
+ * Remainder %
+ */
+
+const a = 10;
+const b = 20;
+
+console.log(12 >= a && 12 <= b);
+console.log(12 >= a || 12 <= b);
+console.log(!(a > b));
+
+let foo;
+
+const bar = foo ?? "baz";
+const baz = foo ? "bar" : "baz";
+console.log(bar);
