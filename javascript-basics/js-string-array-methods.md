07b23e1cded7390fc2f0bd3324ebfecc63e7c208
Arpit <arpit_jain@live.com>
2022-04-05 11:09:52 +0530

    feat: array task solutions

--- javascript-basics/js-string-array-methods.md
+++ javascript-basics/js-string-array-methods.md
@@ -11,8 +11,8 @@ Assumption: The string is one word long only.
 1.1. Write a function that takes a string as paramater and returns its sentence case.
 Condition: We will always have the parameter.
 Assumption: The string is one word long only.
-lorem ipsum dolor sit, amet consectetur adipisicing elit. eius, sed commodi molestiae omnis quia quidem aliquam Eos inventore optio velit impedit quasi qui, distinctio et recusandae tenetur quae, sapiente cumque. => Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius, sed commodi molestiae omnis quia quidem aliquam eos inventore optio velit impedit quasi qui, distinctio et recusandae tenetur quae, sapiente cumque.
+lorem ipsum dolor sit, amet consectetur adipisicing elit. eius, sed commodi molestiae omnis quia quidem aliquam Eos inventore optio velit impedit quasi qui, distinctio et recusandae tenetur quae, sapiente cumque. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius, sed commodi molestiae omnis quia quidem aliquam eos inventore optio velit impedit quasi qui, distinctio et recusandae tenetur quae, sapiente cumque.
 
 
 3. Write a function to find that the given number is a prime number.
-3.1 Write a function to find first 20 prime numbers.
+3.1 Write a function to find first n prime numbers.
--- javascript-basics/scripts/string-array-method-task-solution.js
+++ javascript-basics/scripts/string-array-method-task-solution.js
@@ -13,7 +13,74 @@ function isPallindromic(str) {
   return str.toLowerCase() === reversedStr;
 }
 
-console.log(isPallindromic());
+console.log(isPallindromic("sos"));
 console.log(isPallindromic("Sos"));
 console.log(isPallindromic("hello"));
 console.log(isPallindromic("world"));
+
+function capitalizeSentence(str) {
+  const strArray = str.split(" ");
+  const capitalizedWords = [];
+  for (const word of strArray) {
+    const capitalized = capitalize(word);
+    capitalizedWords.push(capitalized);
+  }
+  return capitalizedWords.join(" ")
+}
+
+function convertToSentenceCase(str) {
+  const sentences = str.split(". ");
+  const result = []
+  for (const sentence of sentences) {
+    result.push(sentence[0].toUpperCase() + sentence.slice(1).toLowerCase());
+  }
+  return result.join(". ");
+}
+
+console.log(capitalizeSentence('hello worLD'));
+console.log(
+  capitalizeSentence(
+    "lorem ipsum dolor sit, amet consectetur adipisicing elit. eius, sed commodi molestiae omnis quia quidem aliquam Eos inventore optio velit"
+  )
+);
+
+console.log(
+  convertToSentenceCase(
+    "lorem ipsum dolor sit, amet consectetur adipisicing elit. eius, sed commodi molestiae omnis quia quidem aliquam Eos inventore optio velit impedit quasi qui, distinctio et recusandae tenetur quae, sapiente cumque. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius, sed commodi molestiae omnis quia quidem aliquam eos inventore optio velit impedit quasi qui, distinctio et recusandae tenetur quae, sapiente cumque."
+  )
+);
+
+
+function isPrime(n) {
+  if(n <= 1) {
+    return false
+  }
+  for (let index = 2; index < n; index++) {
+    if(n % index === 0) {
+      return false
+    }
+  }
+  return true
+}
+
+console.log(isPrime(2))
+console.log(isPrime(12))
+console.log(isPrime(19))
+console.log(isPrime(81))
+
+function firstNPrimeNumbers(n) {
+  let count = 0;
+  let num = 2;
+  const primeNumbers = [];
+  while(count < n) {
+    if(isPrime(num)) {
+      count++;
+      primeNumbers.push(num)
+    }
+    num++
+  }
+  return primeNumbers;
+}
+
+console.log(firstNPrimeNumbers(10));
+console.log(firstNPrimeNumbers(20));
